8dfe988ea9cf5fc4b41ae6bba6e2e4a6ce90738e
shinhyojeong <shinhyojeong.w@gmail.com>
2024-01-22 20:34:19 +0900

    config: gatsby-plugin-netlify  설정 추가

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -17,6 +17,7 @@ const config: GatsbyConfig = {
   plugins: [
     'gatsby-plugin-postcss',
     'gatsby-plugin-image',
+    'gatsby-plugin-netlify',
     {
       resolve: 'gatsby-source-contentful',
       options: {
