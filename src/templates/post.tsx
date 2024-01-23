5e08256352da39c7bf46a69bd7fdc5900e3f4f72
shinhyojeong <shinhyojeong.w@gmail.com>
2024-01-24 00:22:34 +0900

    design: 게시글 최대 너비 지정

--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -21,21 +21,23 @@ const post = ({ pageContext }: PageProps<object, PageContext>) => {
 
   return (
     <Layout>
-      <div className="pb-8 pt-12">
-        <h1>{pageContext.post.title}</h1>
-        <p className="mt-4 text-gray-500">
-          {formatDate(pageContext.post.createdAt)}
-        </p>
-      </div>
-      <Divider margin="my-2" />
-      <div className="mb-8 mt-2">
-        <div
-          className="markdown-wrapper"
-          dangerouslySetInnerHTML={{ __html: pageContext.post.content }}
-        />
-      </div>
-      <div className="mt-8">
-        <Tag>{pageContext.post.tag}</Tag>
+      <div className="mx-auto max-w-3xl">
+        <div className="pb-8 pt-12">
+          <h1>{pageContext.post.title}</h1>
+          <p className="mt-4 text-gray-500">
+            {formatDate(pageContext.post.createdAt)}
+          </p>
+        </div>
+        <Divider margin="my-2" />
+        <div className="mb-8 mt-2">
+          <div
+            className="markdown-wrapper"
+            dangerouslySetInnerHTML={{ __html: pageContext.post.content }}
+          />
+        </div>
+        <div className="mt-8">
+          <Tag>{pageContext.post.tag}</Tag>
+        </div>
       </div>
     </Layout>
   )
