6cc97a4f82d2f115c0c1b43c01285cd8c5f1f15d
shinhyojeong <shinhyojeong.w@gmail.com>
2024-02-01 21:12:17 +0900

    feat: Header 아래로 스크롤시 사라지도록 수정

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,44 @@
-export const Header = () => (
-  <div className="fixed left-0 top-0 h-14 w-full border-b border-solid border-gray-200 bg-white">
-    <div className="m-auto h-full max-w-5xl px-2">
-      <a className="flex h-full w-fit items-center" href="/">
-        <span className="text-2xl font-bold">신효정블로그🎶</span>
-      </a>
+import { useEffect, useRef, useState } from 'react'
+
+export const Header = () => {
+  const prevScrollYRef = useRef(0)
+  const [visible, setVisible] = useState(true)
+
+  const headerStyle = {
+    transition: 'transform 500ms ease-in-out',
+    transform: visible ? 'translateY(0)' : 'translateY(-100%)'
+  }
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
+      const { current: prevScrollY } = prevScrollYRef
+
+      setVisible(
+        (prevScrollY > currentScrollY && currentScrollY > 0) ||
+          currentScrollY === 0
+      )
+
+      prevScrollYRef.current = currentScrollY
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.addEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <div
+      className={`fixed left-0 top-0 h-14 w-full transform border-b border-solid border-gray-200 bg-white transition-transform duration-300 ease-in-out ${
+        visible ? 'translate-y-0' : '-translate-y-full'
+      }
+      `}
+      style={headerStyle}>
+      <div className="m-auto h-full max-w-5xl px-2">
+        <a className="flex h-full w-fit items-center" href="/">
+          <span className="text-2xl font-bold">신효정블로그🎶</span>
+        </a>
+      </div>
     </div>
-  </div>
-)
+  )
+}
