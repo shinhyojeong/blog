0605bf3a1b0bacd364a19ae7242273cf34c35a12
shinhyojeong <shinhyojeong.w@gmail.com>
2024-02-28 22:22:53 +0900

    feat: meta:og 수정

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -35,20 +35,17 @@ export const SEO = ({
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
-      <meta property="og:url" content={seo.url} data-gatsby-head="true" />
-      <meta property="og:type" content="website" data-gatsby-head="true" />
-      <meta
-        property="og:site_name"
-        content={seo.title}
-        data-gatsby-head="true"
-      />
-      <meta property="og:title" content={seo.title} data-gatsby-head="true" />
-      <meta
-        property="og:description"
-        content={seo.description}
-        data-gatsby-head="true"
-      />
-      <meta property="og:image" content={seo.image} data-gatsby-head="true" />
+
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content={seo.title} />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:locale" content="ko" />
+      <meta property="og:image" content={seo.image} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
