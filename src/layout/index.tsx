2acf3539d4a47cbaf6adae229b5c6a66355ca2c3
shinhyojeong <shinhyojeong.w@gmail.com>
2024-02-28 21:40:26 +0900

    package: react-helmet 제거

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,4 +1,3 @@
-import { Helmet } from 'react-helmet'
 import { Header } from '~/components'
 import type { PropsWithChildren } from 'react'
 
@@ -7,16 +6,8 @@ type Props = PropsWithChildren<unknown> & {
 }
 
 export const Layout = ({ children, className }: Props) => (
-  <>
-    <Helmet>
-      <html lang="ko" />
-      <title>shinhyojeong.dev</title>
-      <meta content={`Shinhyojeong's Dev Blog`} name="description" />
-      <link href="/favicon.ico" rel="shortcut icon" />
-    </Helmet>
-    <main className={className}>
-      <Header />
-      <div className="mx-auto my-14 max-w-5xl px-2">{children}</div>
-    </main>
-  </>
+  <main className={className}>
+    <Header />
+    <div className="mx-auto my-14 max-w-5xl px-2">{children}</div>
+  </main>
 )
