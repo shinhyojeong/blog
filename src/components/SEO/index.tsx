4f49425bf6abfbf9afe7eb1425aed38e8507960b
shinhyojeong <shinhyojeong.w@gmail.com>
2024-02-28 22:14:42 +0900

    feat: meta:og 추가

--- src/components/SEO/index.tsx
+++ src/components/SEO/index.tsx
@@ -26,7 +26,7 @@ export const SEO = ({
   const seo = {
     title: title || defaultTitle,
     description: description || defaultDescription,
-    image: image || defaultImage,
+    image: image || `${siteUrl}${defaultImage}`,
     url: `${siteUrl}/${pathname}`
   }
 
@@ -35,6 +35,20 @@ export const SEO = ({
       <title>{seo.title}</title>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
+      <meta property="og:url" content={seo.url} data-gatsby-head="true" />
+      <meta property="og:type" content="website" data-gatsby-head="true" />
+      <meta
+        property="og:site_name"
+        content={seo.title}
+        data-gatsby-head="true"
+      />
+      <meta property="og:title" content={seo.title} data-gatsby-head="true" />
+      <meta
+        property="og:description"
+        content={seo.description}
+        data-gatsby-head="true"
+      />
+      <meta property="og:image" content={seo.image} data-gatsby-head="true" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={seo.title} />
       <meta name="twitter:url" content={seo.url} />
