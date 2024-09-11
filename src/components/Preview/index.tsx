9078ba7fe07408d4b123b657525ec61536d28b54
shinhyojeong <shinhyojeong.w@gmail.com>
2024-09-11 22:56:46 +0900

    ui: Preview 날짜 글자 크기 수정

--- src/components/Preview/index.tsx
+++ src/components/Preview/index.tsx
@@ -14,7 +14,7 @@ export const Preview = ({ title, content, tag, date, link }: PreviewProps) => (
   <a href={link}>
     <Box className="p-6 transition duration-300 ease-in-out hover:transition-shadow">
       <div className="cursor-pointer">
-        <p className="text-base text-gray-500">{date}</p>
+        <p className="text-sm text-gray-500">{date}</p>
         <div className="mt-1">
           <h2 className="mb-2">{title}</h2>
           <p className="leading-3.5 line-clamp-3 text-ellipsis text-sm text-slate-600">
